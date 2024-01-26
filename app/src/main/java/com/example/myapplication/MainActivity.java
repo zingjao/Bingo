ef22f3d41867c63c1b19c1f541b010cdf53b109a
zingjao <chu_jin02@yahoo.com.tw>
2024-01-26 16:35:18 +0800

    0126賓果調整

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,78 +3,126 @@ package com.example.myapplication;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.Toast;
 
 import java.util.*;
 
 public class MainActivity extends AppCompatActivity {
-    private List<Integer> RandomNumList = new ArrayList<Integer>();
-    private Boolean[] b = {false,false,false,false,false,
-                            false,false,false,false};
-    private Button BingoBtn1,BingoBtn2,BingoBtn3,BingoBtn4,BingoBtn5,
-                    BingoBtn6,BingoBtn7,BingoBtn8,BingoBtn9,RandomNumBtn;
-    private int lines=0
+    //存放數字按鈕陣列
+    private ArrayList<Button> bingoBtnList = new ArrayList<>();
+    private int[][] matrix;
+    private int lines=0,layers=3;
+    private Button RandomNumBtn;
+    private TableLayout bingoNumLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         setUpOnBind();
         setUpOnClick();
-        if(checkBingoLine()) showWinDialog();
+        generateBingoTable(layers);
+//        if(checkBingoLine()) showWinDialog();
     }
 
-    public void setUpOnBind(){
-        BingoBtn1 = findViewById(R.id.BingoBtn1);
-        BingoBtn2 = findViewById(R.id.BingoBtn2);
-        BingoBtn3 = findViewById(R.id.BingoBtn3);
-        BingoBtn4 = findViewById(R.id.BingoBtn4);
-        BingoBtn5 = findViewById(R.id.BingoBtn5);
-        BingoBtn6 = findViewById(R.id.BingoBtn6);
-        BingoBtn7 = findViewById(R.id.BingoBtn7);
-        BingoBtn8 = findViewById(R.id.BingoBtn8);
-        BingoBtn9 = findViewById(R.id.BingoBtn9);
+    private void setUpOnBind(){
+        bingoNumLayout = findViewById(R.id.bingoNumLayout);
         RandomNumBtn = findViewById(R.id.RandomNumBtn);
     }
 
-    public void setUpOnClick(){
-        BingoBtn1.setOnClickListener((View.OnClickListener) this);
-        BingoBtn2.setOnClickListener((View.OnClickListener) this);
-        BingoBtn3.setOnClickListener((View.OnClickListener) this);
-        BingoBtn4.setOnClickListener((View.OnClickListener) this);
-        BingoBtn5.setOnClickListener((View.OnClickListener) this);
-        BingoBtn6.setOnClickListener((View.OnClickListener) this);
-        BingoBtn7.setOnClickListener((View.OnClickListener) this);
-        BingoBtn8.setOnClickListener((View.OnClickListener) this);
-        BingoBtn9.setOnClickListener((View.OnClickListener) this);
-        RandomNumBtn.setOnClickListener((View.OnClickListener) this);
-//        if(RandomNumBtn.on)
+    private void setUpOnClick(){
+        for(int i=0 ; i<layers*layers ; i++){
+            Button bingoPositionBtn = bingoBtnList.get(i);
+            bingoPositionBtn.setOnClickListener(view->{
+                //設定顏色
+                if(bingoPositionBtn.isSelected()) bingoPositionBtn.setBackgroundResource(R.color.black);
+                else bingoPositionBtn.setBackgroundResource(com.google.android.material.R.color.material_dynamic_neutral70);
+                if(checkBingoLine()) showWinDialog();
+            });
+        }
+        RandomNumBtn.setOnClickListener(view->{
+            setNumOnBtn();
+        });
     }
-    public void onClick(View v) {
-        switch (v.getId()) {
 
-            case R.id.RandomNumBtn: setNumOnBtn(); break;
+    private void setNumOnBtn(){
+        for(int i=0 ; i<layers ; i++){
+            for(int j=0 ; j<layers ;j++){
+                Toast.makeText(getApplicationContext(),"遊戲開始！",Toast.LENGTH_SHORT);
+                //0~8 產生1~25亂數
+                bingoBtnList.get(j+(i*3)).setText(""+(int)(Math.random()*24)+1);
+            }
         }
     }
 
-    public void setNumOnBtn(){
-        for(int i=0 ; i<9 ;i++) RandomNumList.add((int)Math.random()*100/4);
+    private void generateBingoTable(int layer){
+        for(int i=0 ; i<layer ; i++){
+            TableRow bingoTableRow = new TableRow(this);
+            bingoTableRow.setGravity(Gravity.CENTER);
+            //一行產生幾個按鈕
+            for(int j=0 ; j<layer ; j++){
+                Button btn = new Button(this);
+                btn.setGravity(Gravity.CENTER);
+                bingoBtnList.add(btn);
+                bingoTableRow.addView(btn);
+            }
+            //一行的樣式&幾層
+            bingoNumLayout.addView(bingoTableRow,layer);
+        }
     }
 
-    public Boolean checkBingoLine(){
-        //if() lines+1
-        if(lines ==2) return true;
+    private Boolean checkBingoLine(){
+        //橫排、直排、斜排對照
+        int columnCheck=0,rowCheck=0,bevelCheck=0;
+        //橫排(i對照直的index j對照橫的index)
+        for(int i=0;i<layers;i++){
+            for(int j=0;j<layers;j++){
+                //(3*3=0 1 2 , 3 4 5 , 6 7 8)
+                if(!bingoBtnList.get(j+(i*layers)).isSelected()) break;
+                else columnCheck+=1;
+            }
+            if(columnCheck==layers) lines++;
+            columnCheck=0;
+        }
+        //直排
+        for(int i=0;i<layers;i++){
+            //(3*3=0,3,6 1,4,7 2,5,8)
+            for(int j=0;j<layers;j++){
+                if(!bingoBtnList.get(i+(j*layers)).isSelected()) break;
+                else rowCheck+=1;
+            }
+            if(rowCheck==layers) lines++;
+            rowCheck=0;
+        }
+        //斜排
+        for(int i=0;i<layers;i++){
+            //(3*3 = 0,4,8 2,4,7)
+            //座標( {0,0} , {1,1} , {2,2} & {0,2} , {1,1} , {2,0} )
+            for(int j=0;j<layers;j++){
+               if((i+j) == (layers-1) || i==j) bevelCheck+=1;
+               else break;
+            }
+            if(bevelCheck==layers) lines++;
+            bevelCheck=0;
+        }
+        if(lines >=2) return true;
+        else return false;
     }
 
-    public void showWinDialog(){
+    private void showWinDialog(){
         AlertDialog.Builder winAlertDialog = new AlertDialog.Builder(MainActivity.this);
         winAlertDialog.setTitle("你贏了！");
         winAlertDialog.setMessage("是否繼續？");
         winAlertDialog.setPositiveButton("確定", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
-                setNumOnBtn();
+//                setNumOnBtn();
             }
         });
         winAlertDialog.setNegativeButton("取消", new DialogInterface.OnClickListener() {
